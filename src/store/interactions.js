07c8769d91c781724b694eaa8b8bb7613720f49d
kantorkid <yourgitemail@example.com>
2022-07-03 03:26:08 -0400

    updated account connection

--- src/store/interactions.js
+++ src/store/interactions.js
@@ -38,8 +38,8 @@ export const loadWeb3 = async (dispatch) => {
 
 export const loadAccount = async (web3, dispatch) => {
   const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+  const accounts = await web3.eth.getAccounts();
   const account = web3.utils.toChecksumAddress(accounts[0])
-  const accounts = await web3.eth.getAccounts()
   const account = await accounts[0]
   if(typeof account !== 'undefined'){
     dispatch(web3AccountLoaded(account))
